6f1e50e1e88d2e6710c1f773e8196340f3ce5af4
David Souther <davidsouther@gmail.com>
2014-11-18 10:21:48 -0500

    Basic page shell.

--- src/client/main.js
+++ src/client/main.js
@@ -1,4 +1,10 @@
 angular.module('trkstr', [
   'ngMaterial',
   'trkstr.head-controller'
-]);
+])
+.controller('AppCtrl', function($scope, $timeout, $mdSidenav) {
+  $scope.toggleMenu = function(){
+    $mdSidenav().toggle();
+  };
+})
+;
